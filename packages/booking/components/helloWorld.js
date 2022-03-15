aad7dc4e5bb99b0ee294897342204fa4a9381695
Rodrigo Cabrera <rodrigo_cabrera_cab@hotmail.com>
2022-03-15 00:41:07 -0600

    chore: <ESLint configuration added and tested>

--- packages/booking/components/helloWorld.js
+++ packages/booking/components/helloWorld.js
@@ -3,7 +3,7 @@ import React from "react";
 
 export const Booking = () => {
   return (
-    console.log('Hello from Booking')
+    console.log("Hello from Booking")
     // <button>Hello from Booking</button>
-  )
+  );
 };
